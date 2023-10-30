bedbf8c9bf1e807b63ecfc7ac90ec6d47bffcea9
Jakai Xu <jakaixu@gmail.com>
2023-10-30 17:41:01 +1100

    updated deletePhoto ui

--- src/components/show-case-ui/PhotoItem.tsx
+++ src/components/show-case-ui/PhotoItem.tsx
@@ -1,7 +1,6 @@
 import { PhotoProps } from "./PhotoList";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { DeleteOutlineOutlined } from "@mui/icons-material";
-import { getRandomColor } from "../../utils/randomColor";
 import { useRemovePhotoMutation } from "../../store/store";
 import { LoadingButton } from "@mui/lab";
 
@@ -11,43 +10,42 @@ const PhotoItem = ({ photo }: { photo: PhotoProps }) => {
     removePhoto(photo);
   };
   return (
-    <Box>
+    <Box
+      sx={{ position: "relative", cursor: "pointer" }}
+      onClick={handleRemovePhoto}
+    >
       <Box component="img" sx={styles.img} src={photo.url} />
-      <Stack sx={styles.title}>
-        <Typography sx={styles.label}>{photo.id}</Typography>
+      <Box sx={styles.hoverBox}>
         {
-          <LoadingButton
-            loading={results.isLoading}
-            onClick={handleRemovePhoto}
-          >
-            <DeleteOutlineOutlined />
+          <LoadingButton loading={results.isLoading}>
+            <DeleteOutlineOutlined sx={styles.icon} />
           </LoadingButton>
         }
-      </Stack>
+      </Box>
     </Box>
   );
 };
 
 export default PhotoItem;
 const styles = {
-  title: {
+  img: { width: 200, height: 200, objectFit: "cover" },
+  hoverBox: {
+    position: "absolute",
+    inset: 0,
+    width: 200,
+    height: 200,
+    opacity: 0,
+    "&: hover": {
+      bgcolor: "grey",
+      opacity: 0.6,
+    },
     display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingX: 2,
-    bgcolor: getRandomColor(),
-  },
-  label: {
-    bgcolor: "grey",
-    width: "30px",
-    height: "30px",
-    borderRadius: "50px",
-    display: "flex",
-    flexDirection: "row",
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    opacity: 0.6,
   },
-  img: { width: 200, height: 200, objectFit: "cover" },
+  icon: {
+    fontSize: 48,
+    color: "white",
+  },
 };
